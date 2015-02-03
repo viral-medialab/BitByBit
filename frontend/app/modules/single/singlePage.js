2dd19724c014c05c24f0c20ae826b4e9c21e002f
Natasha Jaques <jaquesn@mit.edu>
2015-02-03 11:38:02 -0500

    sign up form validation first attempt

--- frontend/app/modules/single/singlePage.js
+++ frontend/app/modules/single/singlePage.js
@@ -5,18 +5,32 @@ singlePage.directive("singlePane", function(){
 		restrict: 'E',
 		templateUrl: '/templates/single-pane.html',
 		controller: function($http,$scope){
-            $scope.dateNotChecked=false;
+            $scope.workshops = [false,false,false,false,false];
+            $scope.need = "";
+            $scope.insight = "";
+            $scope.problem_description = ""
 
+            $scope.noWorkshop=false;
+            $scope.noProblem = false;
+            $scope.noNeed = false;
+            $scope.noInsight = false;
+            $scope.invalid = true;
 
-            this.func = function(cat){
-                alert(cat);
+            this.submit = function(need,insight,problem_description,workshops){
+                alert("submitting " + need + insight + problem_description + workshops);
             }
 
             this.validate = function(){
-                alert(cat);
+                $scope.noWorkshop = workshops == [false,false,false,false,false];
+                $scope.noNeed = need == "";
+                $scope.noInsight = insight=="";
+                $scope.noProblem = problem_description=="";
+                $scope.invalid = noNeed || noProblem || noWorkshop || noInsight;
             }
 		},
 		controllerAs: 'singleCntrl'
 	};
 });
+
+
 
