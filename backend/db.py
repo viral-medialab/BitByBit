515469268c8d302f9981606362a96684976f3c9b
Travis Rich <travis.s.rich@gmail.com>
2015-02-12 18:16:43 -0500

    Replyto Update

--- backend/db.py
+++ backend/db.py
@@ -155,6 +155,7 @@ class mongoInstance(object):
 		message.set_html('<html><body><p>'+messageText+'</p> <p>Please visit http://bitxbit.media.mit.edu to update your information.</p><h3>NOTE: This email is completely anonymous.  Our admin interface never reveals your name, email, or any identifying information. Our backend handles all email addresses and we delete all records of having sent this message to you. If you reply to this email - we will of course then know your identity. </h3></body></html>')
 		# message.set_text(messageText)
 		message.set_from('BITxBIT Team <bitxbit@media.mit.edu>')
+		message.set_replyto('slavin@media.mit.edu')
 		status, msg = sg.send(message)
 		return [status, msg]
 
